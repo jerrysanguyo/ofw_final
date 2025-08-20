14442cba99eb65db9ee21cc3e09d84e9a6f1546c
Jerry Sanguyo <jsanguyo1624@gmail.com>
2025-08-20 22:08:05 +0800

    added: realtion to sdebar

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -271,6 +271,14 @@
                                         Type of ID
                                     </a>
                                 </li>
+                                <li
+                                    class="dropdown {{ request()->routeIs(Auth::user()->getRoleNames()->first().'.relation.index') ? 'active' : '' }}">
+                                    <a class="nav-link"
+                                        href="{{ route(Auth::user()->getRoleNames()->first() . '.relation.index') }}">
+                                        <i class="fa-solid fa-person-breastfeeding"></i>
+                                        Relations
+                                    </a>
+                                </li>
                             </ul>
                         </li>
                         @endrole
